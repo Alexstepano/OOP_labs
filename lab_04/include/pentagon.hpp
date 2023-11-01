88b920c9c9d52214fbbd6a9b2e8d12dec5e5d5ff
Alexstepano <aleksey.stepanov2004@mail.ru>
2023-11-01 18:55:47 +0300

    Constant chill deep inside

---
+++ lab_04/include/pentagon.hpp
@@ -0,0 +1,40 @@
+#ifndef PENTAGON_HPP_INCLUDED
+#define PENTAGON_HPP_INCLUDED
+#include "Figure.hpp"
+
+template<typename T>
+class Pentagon final: public Figure<T>
+{
+    Point<T> _a;
+    Point<T> _b;
+    Point<T> _c;
+    Point<T> _d;
+    Point<T> _e;
+    double side;
+
+public:
+    Pentagon() = default;
+    Pentagon(const Point<T> &a, const Point<T> &b, const Point<T> &c, const Point<T> &d, const Point<T> &e);
+    Pentagon(T _side);
+    Pentagon(const Pentagon &other);
+    Pentagon(Pentagon &&other) noexcept;
+    Pentagon &operator=(const Pentagon &other);
+    Pentagon &operator=(Pentagon &&other) noexcept;
+    virtual Point<T> geometric_center() const override;
+    virtual double area() const override;
+    virtual operator double() const override;
+    bool operator==(const Pentagon &other) const;
+    template<typename j>
+    friend std::istream &operator>>(std::istream &in, Pentagon<j> &x);
+    template<typename j>
+    friend std::ostream &operator<<(std::ostream &out, const Pentagon<j> &x);
+    ~Pentagon() = default;
+    virtual void print() const override;
+    virtual void read() override;
+
+private:
+    bool check_Pentagon(const Point<T> &a, const Point<T> &b, const Point<T> &c, const Point<T> &d, const Point<T> &e) const;
+};
+
+#include "../src/Pentagon.cpp"
+#endif// PENTAGON_HPP_INCLUDED
